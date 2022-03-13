ec84822c9695ab8202d43edc19fc1b44083cc189
Lucas Zluhan <lszluhan@hotmail.com>
2022-03-13 00:53:54 -0300

    adicionado novas rotas no router

---
+++ src/pages/contatos/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Contatos = () => {
+  return <React.Fragment></React.Fragment>;
+};
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { HomePage } from '../pages/home';
+import { Registro } from '../pages/registro';
+import { Contatos } from '../pages/contatos';
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/contatos" element={<Contatos />} />
         <Route path="/*" element={<Navigate to={'/'} />} />
       </Routes>
     </BrowserRouter>
